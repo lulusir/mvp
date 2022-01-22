d5eb06d5ddb9f4eb099cb1dd527685e42261c7df
lujs <327240969@qq.com>
2022-01-22 22:53:02 +0800

    update export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Model } from './model/Model';
 export { Presenter } from './presenter/presenter';
 export { entry } from './entry/entry';
-export { injectable, singleton, container } from '@lujs/di';
+export { injectable, singleton, container, inject } from '@lujs/di';
 export { PresenterFactor } from './presenter/factor';
